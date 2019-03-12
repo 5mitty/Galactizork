5258ec04a61465b7f134835bb91b909051943e15
Jacob Smith <jsmith0361@jh-mba158-19.local>
2019-03-12 11:48:07 -0500

    added textView to controller, added welcome screen

--- Galactizork/ViewController.swift
+++ Galactizork/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gameConsoleTextView: UITextView!
+    var gameConsoleText: String = ""
+    var continueGame: Bool = true
+    var completedStoryPart1: Bool = false
+    
+    
     override func viewDidLoad() {
+        print("loaded")
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .blue
+        runGalactizork()
+        
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        print("appeared")
+        //runGalactizork()
+    }
+    
+    func runGalactizork() {
+        if continueGame == true {
+            print("Here")
+            gameConsoleText = "Welcome to Galactizork!"
+            setGameText()
+            
+            runStoryPart1()
+        }
     }
 
+    func runStoryPart1() {
+        
+    }
 
+    func setGameText() {
+        
+        gameConsoleTextView.text +=  "\n\(gameConsoleText)"
+        gameConsoleText = ""
+    }
 }
 
